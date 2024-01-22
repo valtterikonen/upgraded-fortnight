dd1139d7019249ca317a8495f9d3bce06e60ed2b
valtterikonen <valtteri.ikonen@metropolia.fi>
2024-01-22 10:47:05 +0200

    react-basics2

---
+++ jsx-lab/src/SimpleComponent.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// Defining  a simple functional component
+function SimpleComponent() {
+    return (
+        <div>
+            <h1>Hello</h1>
+            <p>Simple React using JSX</p>
+        </div>
+    );
+}
+
+//Export component
+export default SimpleComponent;
